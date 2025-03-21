5513953edc9c69b35142461caac57d4e084579f5
constant6154 <hossainasif6154@gmail.com>
2025-03-21 06:01:07 +0600

    day 2 of making new project

--- QR code Encoder Decoder.py
+++ QR code Encoder Decoder.py
@@ -1,6 +1,17 @@
 import qrcode
+import os
 
 data = 'Porashuna bhalo lagena'
-img = qrcode.make(data)
-img.save('E:/Python/Pycharm/myqr.png')
+save_path = 'E:\QR\mywr.png'
 
+# Ensure the directory exists
+os.makedirs(os.path.dirname(save_path), exist_ok=True)
+
+# Generate QR code
+img = qrcode.make(data)
+
+# Save the image
+img.save(save_path)
+
+# Show the QR code
+img.show()
